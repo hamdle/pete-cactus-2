30684b58680164e4c018c8088a944ffd3ec126bf
Eric Marty <contact@ericmarty.dev>
2020-11-15 12:14:01 -0800

    Add Standard Assets Cross Platform Input.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 7f;
+
+    Rigidbody2D playerRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+    }
+
+    private void Run()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // from -1 to +1
+        playerRigidbody.velocity = new Vector2(controlThrow * runSpeed, playerRigidbody.velocity.y);
+    }
+}
