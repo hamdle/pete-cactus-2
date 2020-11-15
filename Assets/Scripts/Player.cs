ec6a84da327ab507371a2268761271c1f32d0295
Eric Marty <contact@ericmarty.dev>
2020-11-15 12:49:08 -0800

    Add running state change to Player.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,14 +5,21 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
+    // Config
     [SerializeField] float runSpeed = 7f;
 
+    // State
+    bool isAlive = true;
+
+    // Cached component references
     Rigidbody2D playerRigidbody;
+    Animator playerAnimator;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,12 +32,17 @@ public class Player : MonoBehaviour
     private void Run()
     {
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // from -1 to +1
-        playerRigidbody.velocity = new Vector2(controlThrow * runSpeed, playerRigidbody.velocity.y);
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, playerRigidbody.velocity.y);
+        playerRigidbody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(playerRigidbody.velocity.x) > Mathf.Epsilon; // Epsilon == 0
+        playerAnimator.SetBool("Running", playerHasHorizontalSpeed);
     }
 
     private void FlipSprite()
     {
-        if (Mathf.Abs(playerRigidbody.velocity.x) > Mathf.Epsilon) // Epsilon == 0
+        bool playerHasHorizontalSpeed = Mathf.Abs(playerRigidbody.velocity.x) > Mathf.Epsilon; // Epsilon == 0
+        if (playerHasHorizontalSpeed)
         {
             transform.localScale = new Vector2(Mathf.Sign(playerRigidbody.velocity.x), 1f);
         }
